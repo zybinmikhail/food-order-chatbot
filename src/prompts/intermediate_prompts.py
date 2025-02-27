dd149af1a62dd8a180424ddb07755aed4cfbcfc8
Mikhail Zybin <mikhail_zybin@epam.com>
2025-02-27 17:32:31 +0400

    fix grammatical errors in prompts

--- src/prompts/intermediate_prompts.py
+++ src/prompts/intermediate_prompts.py
@@ -4,78 +4,77 @@ I am food order chatbot! My function is to help you choose and order food from v
 
 What would you like to eat today?"""
 
-ask_for_restaurant_dishes_delivery_time = """You are conversation analyzer.
+ask_for_restaurant_dishes_delivery_time = """You are a conversation analyzer.
 
-Please review the conversation between the food ordering chatbot and the user.
+Please review the conversation between the food-ordering chatbot and the user.
 
-Carefully analyze the user's replies and the order summary made by the chatbot.
+Carefully analyze the user's replies and the chatbot's order summary.
 
-Here is the conversation of the chatbot and the user:
+Here is the conversation between the chatbot and the user:
 
 <conversation>
 {}
 </conversation>
 
 Ask yourself the following questions:
-- Has the user already told the chatbot what restaurant they have chosen to have order from?
+- Has the user already told the chatbot what restaurant they have chosen to order from?
 - Has the user already told the chatbot what dishes they have chosen to order? 
-- Has the user already provided chatbot with the information about what delivery time they have chosen?
+- Has the user already provided the chatbot with the information about what delivery time they have chosen?
 
 <instruction>
 1. Identify the suggestions made by the chatbot that the user agreed with
 2. Identify the order summaries made by the chatbot
 3. Identify places when the user says the following words
-  - "No"
-  - "Not really"
-  - "I changed my mind"
-  - "I also want..."
-  - "add..."
-  - "remove..."
+ - "No"
+ - "Not really"
+ - "I changed my mind"
+ - "I also want..."
+ - "add..."
+ - "remove..."
 4. Review the <restaurants-data> and <menus> section of the system_prompt. The user can order only from the restaurants listed in the <restaurants-data> section and only dishes from the <menus> section. 
-5. Create "dish_names" variable. It must store a List of DISTINCT names of the dishes that the user has ordered from the restaurant_name or empty list if the user has not ordered anything yet.
-6. Based on what you identified on the steps 1-3 of this instruction, add to the "dish_names" variable everything that the user ordered.
-7. Create "dish_quantities": List of the number of portions for each dish that the user has ordered or empty list if the user has not ordered anything yet.
-8. Based on what you identified on the steps 1-3 of this instruction, add to the "dish_quantities" variable the number of portions of everything that the user wants to order. The default number of portions is 1. "dish_quantities" list must be of the same length as "dish_names" list.
-9. Create "restaurant_name": Restaurant name that the user has chosen during the conversation or empty if not chosen. This must be one of the restaurants from <restaurants-data> section of the chatbot system prompt.
+5. Create a "dish_names" variable. It must store a List of *distinct* names of the dishes that the user has ordered from the restaurant_name or an empty list if the user has not ordered anything yet.
+6. Based on what you identified in steps 1-3 of this instruction, add to the "dish_names" variable everything that the user ordered.
+7. Create "dish_quantities": A list of the number of portions for each dish that the user has ordered or an empty list if the user has not ordered anything yet.
+8. Based on what you identified in steps 1-3 of this instruction, add to the "dish_quantities" variable the number of portions of everything that the user wants to order. The default number of portions is 1. The "dish_quantities" list must be of the same length as the "dish_names" list.
+9. Create "restaurant_name": Restaurant name that the user has chosen during the conversation or empty if not chosen. This must be one of the restaurants from the <restaurants-data> section of the chatbot system prompt.
 10. Create "delivery_time": Delivery time that the user has chosen during the conversation or empty if not chosen"
-11. Output the values of dish_names, dish_quantities, restaurant_name, delivery_time in json format.
+11. Output the values of dish_names, dish_quantities, restaurant_name, delivery_time in JSON format.
 </instruction>
 
 <guidelines>
 You must strictly and carefully follow these guidelines. After you create your response, double-check it. Make sure all of the guidelines are followed.
 
 - Carefully analyze the user's replies and the order summary made by the chatbot.
-- Has the user already told the chatbot what restaurant they have chosen to have order from?
-- The user can change their order. If the user canceled their order, it means they do not want the dishes from that order.  Newer information has higher priority.
-- If the user has chosen a specific dish, it means they have also chosen the restaurant where this dish comes from. Dish information has higher priority.
+- Has the user already told the chatbot what restaurant they have chosen to order from?
+- The user can change their order. If the user cancels their order, it means they do not want the dishes from that order.  Newer information has higher priority.
+- If the user has chosen a specific dish, it means they have also chosen the restaurant where this dish comes from. Dish information has a higher priority.
 - All dishes must be from the same restaurant that the user has chosen.
 - If the user has already told the chatbot what dishes they have chosen to order, then output the dishes the user ordered and the number of portions for each dish.
-- Your task is to output valid json strictly in output format. Don't output anything else.
-- All the DISTINCT dishes from the "dish_names" output field must come from the same restaurant "restaurant_name".
-- Keep in mind that the user can change their order. Newer information has higher priority. Pay special attention to the last user reply.
+- Your task is to output valid JSON strictly in output format. Don't output anything else.
+- All the *distinct* dishes from the "dish_names" output field must come from the same restaurant "restaurant_name".
+- Keep in mind that the user can change their order. Newer information has higher priority. Pay special attention to the last user's reply.
 - Keep in mind: if the user has chosen a specific dish, it means they have chosen the restaurant where this dish comes from.
-- Carefully analyze the user's replies and the order summary made by the chatbot.
-- Based on the conversation of the chatbot and the user, has the user already provided chatbot with the information about what dishes they want to order? 
-- If the user has not ordered anything specific yet, the fields of your json output must be empty lists. 
+- Based on the conversation between the chatbot and the user, has the user already provided the chatbot with the information about what dishes they want to order? 
+- If the user has not ordered anything specific yet, the fields of your JSON output must be empty lists. 
 - Pay special attention when the user says the following words and double-check your output
-  - "No", 
-  - "Not really"
-  - "I changed my mind"
-  - "I also want..."
-  - "add..."
-  - "remove..."
+ - "No", 
+ - "Not really"
+ - "I changed my mind"
+ - "I also want..."
+ - "add..."
+ - "remove..."
 - The user can change their order. Newer information has higher priority. Pay special attention to requests of the user to add or remove something. All dish names must be exactly present in the menu of one restaurant. All the dishes must come from the same restaurant.
-- Output valid json strictly in output format.
+- Output valid JSON strictly in output format.
 </guidelines>
 
 Output format:
 ```json
 {{
-  "dish_names": "List of DISTINCT names of the dishes that the user has ordered from the restaurant_name or empty list if the user has not ordered anything yet",
-  "dish_quantities": "List of the number of portions for each dish that the user has ordered or empty list if the user has not ordered anything yet",
-  "restaurant_name": "Restaurant name that the user has chosen during the conversation or empty if not chosen",
-  "delivery_time": "Delivery time that the user has chosen during the conversation or empty if not chosen",
-  "explanation": "Maximum 50 words explanation of your output"
+ "dish_names": "List of *distinct* names of the dishes that the user has ordered from the restaurant_name or empty list if the user has not ordered anything yet",
+ "dish_quantities": "List of the number of portions for each dish that the user has ordered or an empty list if the user has not ordered anything yet",
+ "restaurant_name": "Restaurant name that the user has chosen during the conversation or empty if not chosen",
+ "delivery_time": "Delivery time that the user has chosen during the conversation or empty if not chosen",
+ "explanation": "Maximum 50 words explanation of your output"
 }}
 ```
 
@@ -203,6 +202,10 @@ Expected output:
 
 </examples>
 
+<input>
+{}
+</input>
+
 Output format:
 ```json
 {{
@@ -210,6 +213,6 @@ Output format:
 }}
 ```
 
-Your task is to output only valid json strictly in output format.
-Don't output any python code. Output only the json dictionary.
+Your task is to output only valid JSON strictly in output format.
+Don't output any python code. Output only the JSON dictionary.
 """
